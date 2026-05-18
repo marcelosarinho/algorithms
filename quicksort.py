cd076cdf18e8482deb169eff90e9064fc07bb7fa
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-05-18 16:37:32 -0300

    feat: quicksort algorithm

--- quicksort.py
+++ quicksort.py
@@ -1,11 +1,11 @@
 def quicksort(arr):
-  if len(arr < 2):
+  if len(arr) < 2:
     return arr
 
   pivot = arr[0]
   lowerThanPivot = [i for i in arr[1:] if i <= pivot]
   higherThanPivot = [i for i in arr[1:] if i > pivot]
-  return quicksort(lowerThanPivot) + pivot + quicksort(higherThanPivot)
+  return quicksort(lowerThanPivot) + [pivot] + quicksort(higherThanPivot)
 
 print(quicksort([10, 5, 2, 3]))
 print(quicksort([1, 35, 22, 345]))
